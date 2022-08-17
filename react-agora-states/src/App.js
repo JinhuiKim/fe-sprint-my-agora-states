ac90d3350cef28baa468c99b76d8044958a66b1f
JinhuiKim <public.jinhui.kim@gmail.com>
2022-08-17 16:54:15 +0900

    Feat: 제출버튼 클릭 시 localstorage 저장

--- react-agora-states/src/App.js
+++ react-agora-states/src/App.js
@@ -1,15 +1,55 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import { FormContainer } from "./FormContainer";
 import { Pagenation } from "./Pagenation";
 import { DiscussionWrapper } from "./DiscussionWrapper";
+import { getFindAll } from "./api/DiscussionsDataApi";
 
 const App = () => {
+  const [discussions, setDiscussions] = useState([]);
+  const [submitCheck, setSubmitCheck] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getFindAll();
+      const localStorageJsonArray = getLocalStorageData();
+
+      let discussions = [];
+      if (localStorageJsonArray) {
+        discussions = [...localStorageJsonArray, ...response];
+      } else {
+        discussions = [...response];
+      }
+      setDiscussions(discussions);
+    };
+
+    fetchData();
+  }, [submitCheck]);
+
+  const getLocalStorageData = () => {
+    return JSON.parse(localStorage.getItem("discussion_array"));
+  };
+
+  const onClickSubmitBtn = (discussionObj) => {
+    console.log(discussionObj);
+    const localStorageJsonArray = getLocalStorageData();
+    let JsonArray = [];
+    if (localStorageJsonArray) {
+      JsonArray = [discussionObj, ...localStorageJsonArray];
+    } else {
+      JsonArray = [discussionObj];
+    }
+
+    localStorage.setItem("discussion_array", JSON.stringify(JsonArray));
+    setSubmitCheck(!submitCheck);
+  };
+
   return (
     <main>
       <h1>My Agora States</h1>
-      <FormContainer />
-      <DiscussionWrapper />
-      <Pagenation />
+      <FormContainer onClickSubmitBtn={onClickSubmitBtn} />
+      <DiscussionWrapper discussions={discussions} />
+      <Pagenation discussions={discussions} />
     </main>
   );
 };
--- react-agora-states/src/FormContainer.js
+++ react-agora-states/src/FormContainer.js
@@ -1,9 +1,36 @@
 import React from "react";
 
-export const FormContainer = () => {
+export const FormContainer = (props) => {
+  const { onClickSubmitBtn } = props;
+  const setBodyHTML = (text) => {
+    let lines = text.split("\n");
+    let resultString = "";
+    for (let i = 0; i < lines.length; i++) {
+      resultString += lines[i] + "<br>\n";
+    }
+    return <p dir="auto">${resultString}</p>;
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const { target } = event;
+    const discussionObj = {
+      id: `D_${Math.random().toString(36).substring(2)}`,
+      createdAt: new Date().toISOString(),
+      title: target[1].value,
+      url: "#",
+      author: target[0].value,
+      answer: null,
+      bodyHTML: setBodyHTML(target[2].value),
+      avatarUrl: "https://joeschmoe.io/api/v1/random",
+    };
+
+    onClickSubmitBtn(discussionObj);
+  };
+
   return (
     <section className="form__container">
-      <form action="" method="get" className="form">
+      <form action="" method="get" className="form" onSubmit={onSubmit}>
         <div className="form__input--wrapper">
           <div className="form__input--name">
             <label htmlFor="name">Enter your name: </label>
